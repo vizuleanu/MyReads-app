8aee60d1bb0de30fc4913057cf19f19e66031100
vizuleanu <paliciucdan99@gmail.com>
2018-07-16 22:19:02 +0300

    add Title component

---
+++ src/components/Title.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const Title = (p) => {
+    return(
+        <div className="list-books-title">
+            <h1>{p.name}</h1>
+        </div>
+    )
+}
+
+Title.defaultProps = {
+    name: 'My Reads'
+}
+
+Title.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
+export default Title
